3e747893fb7d4b659c62d7c30c9e154933be5ab5
WePlayFleX <alex.kamai@ya.ru>
2022-01-24 23:07:22 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCatchMe = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCatchMe
+            // 
+            this.btnCatchMe.Location = new System.Drawing.Point(69, 36);
+            this.btnCatchMe.Name = "btnCatchMe";
+            this.btnCatchMe.Size = new System.Drawing.Size(125, 23);
+            this.btnCatchMe.TabIndex = 0;
+            this.btnCatchMe.Text = "Поймай меня";
+            this.btnCatchMe.UseVisualStyleBackColor = true;
+            this.btnCatchMe.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Form
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCatchMe);
+            this.Name = "Form";
+            this.Text = "Main Form";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCatchMe;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        private Random random;
+
+        public Form()
+        {
+            InitializeComponent();
+            random = new Random();
+        }
+
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            //if (btnCatchMe.Location.X < ClientSize.Width && btnCatchMe.Location.Y < ClientSize.Height)
+            //{
+            //    btnCatchMe.Location = new Point(random.Next(this.Width), random.Next(this.Height));
+            //}
+
+            //var result = MessageBox.Show("Test", "Message", MessageBoxButtons.YesNo,
+            //    MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+
+            //if (result == DialogResult.Yes)
+            //{
+            //    MessageBox.Show("Да");
+            //}
+            //else if (result == DialogResult.No)
+            //{
+            //    MessageBox.Show("Нет");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Закрыл");
+            //}
+
+            
+
+
+
+
+
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+
+            //string str1 = "Hello";
+            //string str2 = "My name Aliaksandr";
+            //string str3 = "I from Minsk";
+            //int lenght = str1.Length + str2.Length + str3.Length;
+
+            //MessageBox.Show(str1);
+            //MessageBox.Show(str2);
+            //MessageBox.Show(str3);
+            //MessageBox.Show("Количество символов: " + lenght);
+
+            int minNumber = 0;
+            int maxNumber = 10;
+            int searchNumber;
+
+            while (true)
+            {
+                var result = MessageBox.Show("Search number ", "Число меньше " + maxNumber / 2, MessageBoxButtons.YesNoCancel);
+
+                if (result == DialogResult.Yes)
+                {
+                    maxNumber = maxNumber / 2;
+                }
+                else if (result == DialogResult.No)
+                {
+                    minNumber = maxNumber / 2;
+                }
+                else
+                {
+                    MessageBox.Show("Загаданное число: " + minNumber);
+                    break;
+                }
+            }
+        }
+    }
+}
